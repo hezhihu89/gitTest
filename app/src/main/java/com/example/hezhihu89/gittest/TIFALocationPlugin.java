1fb205d65a3969a06d4eb8f9684ba710d544c6ad
hezhihu89 <354137379@qq.com>
2016-01-03 03:02:11 +0800

    tijiaoceshi

---
+++ app/src/main/java/com/example/hezhihu89/gittest/TIFALocationPlugin.java
@@ -0,0 +1,49 @@
+package com.example.hezhihu89.gittest;
+
+import android.content.Context;
+
+/**
+ * @项目名 ：GooglePlay
+ * @包名 ：com.example.hezhihu89.gittest
+ * @User ： hezhihu89 by：贺志虎
+ * @创建时间 ：2015 年 12 月 30 日   15时 17分.
+ * @类的描述 : TODO：
+ */
+public class TIFALocationPlugin {
+
+    Context mContext;
+
+    /**
+     * 初始化
+     */
+    public TIFALocationPlugin() {
+
+    }
+
+    public TIFALocationPlugin(Context content) {
+        this.mContext = mContext;
+    }
+
+    /**
+     * 获取城市
+     *
+     * @return
+     */
+    public String getCity() {
+
+        return "";
+    }
+
+    /**
+     * 获取经纬度
+     *
+     * @return
+     */
+    public String getLocation() {
+
+
+        return "";
+    }
+
+
+}
